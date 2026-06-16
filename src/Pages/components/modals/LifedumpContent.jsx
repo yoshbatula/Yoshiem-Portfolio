01bbd0144fd70acbfa6ac516a1619a8dc5c9646c
yoshbatula <yoshbatula2@gmail.com>
2026-06-16 15:44:45 +0800

    adding lifedump photos

--- src/Pages/components/modals/LifedumpContent.jsx
+++ src/Pages/components/modals/LifedumpContent.jsx
@@ -1,4 +1,4 @@
-const photos = Array.from({ length: 24 }, (_, i) => ({
+const photos = Array.from({ length: 35 }, (_, i) => ({
   id: i + 1,
   rotate: ['-rotate-1', 'rotate-1', '-rotate-2', 'rotate-2', 'rotate-0', '-rotate-1', 'rotate-1', 'rotate-0'][i % 8],
   tape: [true, false, false, true, false, true, false, false][i % 8],
@@ -17,6 +17,18 @@ const gradients = [
   'from-orange-500 to-pink-500', 'from-teal-500 to-lime-500',
   'from-indigo-500 to-amber-500', 'from-red-500 to-yellow-500',
   'from-purple-500 to-teal-500', 'from-blue-500 to-rose-500',
+  'from-cyan-500 to-lime-500', 'from-fuchsia-500 to-orange-500',
+  'from-emerald-500 to-violet-500', 'from-yellow-500 to-indigo-500',
+  'from-pink-500 to-teal-500', 'from-sky-500 to-purple-500',
+  'from-amber-500 to-blue-500', 'from-lime-500 to-fuchsia-500',
+  'from-teal-500 to-rose-500', 'from-orange-500 to-cyan-500',
+  'from-violet-500 to-yellow-500', 'from-red-500 to-teal-500',
+  'from-blue-500 to-pink-500', 'from-purple-500 to-lime-500',
+  'from-cyan-500 to-amber-500', 'from-rose-500 to-sky-500',
+  'from-lime-500 to-indigo-500', 'from-fuchsia-500 to-teal-500',
+  'from-green-500 to-purple-500', 'from-yellow-500 to-cyan-500',
+  'from-sky-500 to-rose-500', 'from-amber-500 to-violet-500',
+  'from-emerald-500 to-fuchsia-500',
 ]
 
 export default function LifedumpContent() {
